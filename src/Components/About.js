77152727a067ff47cc6ade92e79c62b66c09dd9c
Kiup Kim <kiupkim@Kiups-MacBook-Air.local>
2023-10-29 01:19:56 -0600

    change the order

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,6 @@ class About extends Component {
     if(this.props.data){
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
@@ -25,7 +24,6 @@ class About extends Component {
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,6 @@ class Header extends Component {
     if(this.props.data){
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
@@ -33,7 +32,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>{description}.</h3>
+            <h3>I'm a {city} based <span>{occupation}</span>.</h3>
             <hr />
             <ul className="social">
                {networks}
